21253736787c8083cd4f72c70ef6c08e0f4eadf4
Abdul-latif <latifm8360@gmail.com>
2021-01-29 21:11:40 -0800

    js file added

---
+++ index.js
@@ -0,0 +1,16 @@
+function previewfile(){
+			var visible=document.getElementById('preview');
+			var preview=document.querySelector('img');
+			var file=document.querySelector('input[type=file]').files[0];
+			var reader=new FileReader();
+			reader.onloadend=function(){
+				preview.src=reader.result;
+			}
+			if(file){
+				reader.readAsDataURL(file);
+				visible.style.display="block";
+			} else{
+				preview.src="";
+				visible.style.display="none"
+			}
+		}
